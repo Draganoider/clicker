9efdc27e4645259ed36dc0cdbae7b7149e160c66
Draganoider <evgen.draga@gmail.com>
2024-06-07 12:38:05 -0700

    Add project files.

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace clicker
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBoxY = new System.Windows.Forms.TextBox();
+			this.textBoxX = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxClicks = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBoxDelay = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(347, 325);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBoxY
+			// 
+			this.textBoxY.Location = new System.Drawing.Point(70, 225);
+			this.textBoxY.Name = "textBoxY";
+			this.textBoxY.Size = new System.Drawing.Size(100, 22);
+			this.textBoxY.TabIndex = 1;
+			// 
+			// textBoxX
+			// 
+			this.textBoxX.Location = new System.Drawing.Point(70, 181);
+			this.textBoxX.Name = "textBoxX";
+			this.textBoxX.Size = new System.Drawing.Size(100, 22);
+			this.textBoxX.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(49, 181);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(15, 16);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "X";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(49, 225);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 16);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Y";
+			// 
+			// textBoxClicks
+			// 
+			this.textBoxClicks.Location = new System.Drawing.Point(641, 184);
+			this.textBoxClicks.Name = "textBoxClicks";
+			this.textBoxClicks.Size = new System.Drawing.Size(100, 22);
+			this.textBoxClicks.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(747, 187);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 16);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Clicks";
+			// 
+			// textBoxDelay
+			// 
+			this.textBoxDelay.Location = new System.Drawing.Point(641, 225);
+			this.textBoxDelay.Name = "textBoxDelay";
+			this.textBoxDelay.Size = new System.Drawing.Size(100, 22);
+			this.textBoxDelay.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(745, 231);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(43, 16);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Delay";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::clicker.Properties.Resources._5a5e5b230aefb161009400e5;
+			this.pictureBox1.Location = new System.Drawing.Point(296, 58);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(187, 145);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 9;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBoxDelay);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBoxClicks);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxX);
+			this.Controls.Add(this.textBoxY);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBoxY;
+		private System.Windows.Forms.TextBox textBoxX;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxClicks;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBoxDelay;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ clicker/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        // Определяем структуру для хранения информации о вводе
+        struct INPUT
+        {
+            public uint Type;
+            public MOUSEINPUT MouseInput;
+        }
+
+        // Определяем структуру для хранения информации о клике мыши
+        struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        // Константы для имитации клика мыши
+        const uint INPUT_MOUSE = 0x0000;
+        const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const uint MOUSEEVENTF_LEFTUP = 0x0004;
+
+        private bool isStopped = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Добавляем обработчик события нажатия клавиш
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Проверяем, была ли нажата клавиша Q для аварийной остановки
+            if (e.KeyCode == Keys.Q)
+            {
+                isStopped = true;
+            }
+
+            // Проверяем, были ли нажаты клавиши Ctrl и Shift, и клавиша D
+            if (e.Control && e.Shift && e.KeyCode == Keys.D)
+            {
+                // Вызываем метод PerformClick для кнопки button1
+                button1.PerformClick();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем значения из текстовых полей
+            int numClicks = int.Parse(textBoxClicks.Text);
+            int delay = int.Parse(textBoxDelay.Text);
+
+            // Задержка в 3 секунды перед началом кликов
+            Thread.Sleep(3000);
+
+            // Выполняем клики
+            for (int i = 0; i < numClicks; i++)
+            {
+                if (isStopped)
+                {
+                    MessageBox.Show("AutoClicker stopped");
+                    return;
+                }
+
+                // Получаем текущую позицию курсора мыши
+                Point cursorPosition = Cursor.Position;
+
+                // Имитируем клик левой кнопкой мыши
+                INPUT input = new INPUT();
+                input.Type = INPUT_MOUSE;
+                input.MouseInput.X = cursorPosition.X;
+                input.MouseInput.Y = cursorPosition.Y;
+                input.MouseInput.Flags = MOUSEEVENTF_LEFTDOWN;
+                SendInput(1, new INPUT[] { input }, Marshal.SizeOf(input));
+                input.MouseInput.Flags = MOUSEEVENTF_LEFTUP;
+                SendInput(1, new INPUT[] { input }, Marshal.SizeOf(input));
+
+                // Задержка перед следующим кликом
+                Thread.Sleep(delay);
+            }
+
+            // Выводим сообщение о завершении работы автокликера
+            MessageBox.Show("AutoClicker finished");
+        }
+    }
+}
